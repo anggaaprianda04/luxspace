7e8556160324709f28fe698a1462f3b1880e3d92
anggaaprianda04 <anggaaprianda04@gmail.com>
2023-09-02 15:35:03 +0700

    set up folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import './App.css'
-
 function App() {
-
   return (
     <>
-      <h1 className='text-red-500 text-6xl'>Hello World</h1>
+      <h1 className='text-xl text-red-500'>Hello World</h1>
     </>
   )
 }
